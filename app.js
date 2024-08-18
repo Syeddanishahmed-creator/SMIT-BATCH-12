6b62e4bf8297fe9839780740d3b89c92691d38f1
Syed Danish Ahmed <143102837+Syeddanishahmed-creator@users.noreply.github.com>
2024-08-18 17:26:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+// let  street, country
+
+// street = "main street"
+// country = "Pakistan"
+
+// fullMailingAddress = `${street}  ${country}`
+
+// console.log(fullMailingAddress);
+
+
+const score1 = 80;
+const score2 = 90;
+const score3 = 85;
+
+const total_score = score1+score2+score3
+const average_score = (score1+score2+score3) /3
+
+console.log(average_score);
+
+const plates = 20
+const peoples = 7
+let remaining = plates%peoples
+console.log(remaining);
+++remaining ;
+const message = `there are ${remaining} plates available`
+const message1 = 'there are ' + remaining + ' plates available'
+
+console.log(message);
+
+
